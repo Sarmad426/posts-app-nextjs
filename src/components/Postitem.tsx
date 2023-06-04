0eedd2d58d44be43f7a0605f28b1c928fae4b603
Sarmad rafique <sarmadrafique040@gmail.com>
2023-06-04 10:30:25 +0500

    Changes the component name because of the project nature

--- src/components/Postitem.tsx
+++ src/components/Postitem.tsx
@@ -2,17 +2,30 @@
 import ThumbIcon from "../assets/thumb icon.svg";
 import Image from "next/image";
 import Like from "../assets/like.svg";
+import dislike1 from "../assets/unlike.svg";
+import dislike2 from "../assets/unlike black.svg";
 import { useRouter } from "next/navigation";
+
 interface Post {
   title: string;
   content: string | null;
   id: string;
   Liked: boolean;
+  dislike: boolean;
   updatedAt: Date;
   createdAt: Date;
   handleLike: (id: string) => void;
+  handleDislike: (id: string) => void;
 }
-const TodoItem = ({ id, title, content, Liked, handleLike }: Post) => {
+const TodoItem = ({
+  id,
+  title,
+  content,
+  Liked,
+  dislike,
+  handleLike,
+  handleDislike,
+}: Post) => {
   const router = useRouter();
   return (
     <div className="bg-white rounded-md py-6 px-8 lg:w-96 sm:w-96 text-start my-4 cursor-pointer shadow-xl hover:shadow-md lg:mx-10 sm:mx-5">
@@ -32,6 +45,19 @@ const TodoItem = ({ id, title, content, Liked, handleLike }: Post) => {
             <Image src={Like} alt="Like" width={30} />
           ) : (
             <Image src={ThumbIcon} alt="Like" width={30} />
+          )}
+        </button>
+        <button
+          className="text-white rounded-md mt-5 mb-0 ml-5"
+          onClick={() => {
+            handleDislike(id);
+            return router.refresh();
+          }}
+        >
+          {!dislike || Liked ? (
+            <Image src={dislike1} alt="Like" width={30} />
+          ) : (
+            <Image src={dislike2} alt="Like" width={30} />
           )}
         </button>
       </div>
